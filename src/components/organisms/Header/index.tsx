989e2ecacf846c8be7656e141da4cf46065070d3
Shun Nakano <nakano_syun@waku-2.com>
2021-03-05 14:30:28 +0900

    feat: Headerコンポーネントの作成

---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Header: React.VFC = () => (
+  <header className="py-4 px-8 bg-gray-200">
+    <h1 className="text-4xl font-bold">NewsApp</h1>
+  </header>
+);
