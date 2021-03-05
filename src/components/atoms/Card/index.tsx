4e72648674dfd313fde380b41acc1851e78bb1fb
Shun Nakano <nakano_syun@waku-2.com>
2021-03-05 14:31:22 +0900

    refactor: Cardコンポーネントにprops適用

--- src/components/atoms/Card/index.tsx
+++ src/components/atoms/Card/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import {Articles} from '../../../libs/axios';
 
-export const Card = () => {
+type Props = {
+  article: Articles;
+};
+export const Card: React.VFC<Props> = ({article}) => {
   return (
     <section className="w-72 shadow-md">
-      <a href="" className="block w-full h-full hover:opacity-80">
+      <a href={article.url} className="block w-full h-full hover:opacity-80">
         <div>
-          <img src="/77292743.jpeg" alt="" />
+          <img src={article.urlToImage} alt="" />
         </div>
         <div className="flex flex-col px-2 py-4">
-          <h1>
-            タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
-          </h1>
-          <time className="mt-4 text-gray-400">2021/2/2</time>
-          <cite className="text-gray-400 not-italic">著者</cite>
+          <h1>{article.title}</h1>
+          <time className="mt-4 text-gray-400">{article.publishedAt}</time>
+          <cite className="text-gray-400 not-italic">
+            {article.author || 'Anonymous'}
+          </cite>
         </div>
       </a>
     </section>
